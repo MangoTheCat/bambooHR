e5a27af72464c0464c072d23502b83742dc4d083
tfry <tim.fry@mango-solutions.com>
2022-01-26 09:57:45 +0000

    add purrr when to tidy get_meta response output

--- R/get_meta.R
+++ R/get_meta.R
@@ -17,12 +17,11 @@
 #' }
 #'
 #' @references \url{https://documentation.bamboohr.com/reference/account-information-1}
-get_meta <- function(query, url_args = NULL) {
+get_meta <- function(query = c("fields", "tables", "lists", "users", "time_off/types", "time_off/policies"), url_args = NULL) {
   if (!is.null(url_args)) {
     stopifnot(is.list(url_args))
   }
 
-  stopifnot(query %in% c("fields", "tables", "lists", "users", "time_off/types", "time_off/policies"))
   if (length(query) > 1) stop('query must be one of c("fields", "tables", "lists", "users", "time_off/types", "time_off/policies")')
 
   if (!is.null(url_args)) {
@@ -32,50 +31,61 @@ get_meta <- function(query, url_args = NULL) {
   }
   url <- do.call(build_url, url_args)
   url <- glue::glue("{url}/meta/{query}")
-  response <- get_request(url)
 
-  if (query == "fields") {
-    return(
-      httr::content(response, as = "text", type = "json", encoding = "UTF-8") |>
-        jsonlite::fromJSON(simplifyDataFrame = TRUE)
+  browser()
+  response <- get_request(url) %>%
+    httr::content(response, as = "text", type = "json", encoding = "UTF-8") %>%
+    purrr::when(
+      query %in% c("fields", "users", "time_off/types", "time_off/policies") ~ jsonlite::fromJSON(. , simplifyDataFrame = TRUE),
+      query %in% c("tables", "lists") ~ jsonlite::fromJSON(. , simplifyDataFrame = FALSE)
+    ) %>%
+    purrr::when(
+      query == "tables" ~ {function(x) {purrr::map_chr(., "alias") %>%
+        tibble::tibble(.) %>%
+        purrr::set_names("parent_alias")%>%
+        dplyr::mutate(fields = purrr::map(x, "fields")) %>%
+        tidyr::unnest_longer(fields) %>%
+        tidyr::unnest_wider(col = "fields")}}(),
+      ~ dplyr::bind_rows(.)
     )
-  } else if (query == "users") {
-    return(
-      httr::content(response, as = "text", type = "json", encoding = "UTF-8") |>
-        jsonlite::fromJSON(simplifyDataFrame = TRUE) |>
-        data.table::rbindlist(fill = TRUE) |>
-        as.data.frame()
-    )
-  } else if (query == "tables") {
-    tbl <- httr::content(response, as = "text", type = "json", encoding = "UTF-8") |>
-      jsonlite::fromJSON(simplifyDataFrame = FALSE)
 
-    return(
-      purrr::map_chr(tbl, "alias") |>
-        tibble::tibble() |>
-        dplyr::mutate(fields = purrr::map(tbl, "fields")) |>
-        tidyr::unnest_longer(fields) |>
-        tidyr::hoist(
-          .col = fields,
-          col_alias = "alias",
-          col_name = "name",
-          col_id = "id",
-          col_type = "type"
-        ) |>
-        as.data.frame()
-    )
-  } else if (query == "lists") {
-    return(
-      httr::content(response, as = "text", type = "json", encoding = "UTF-8") |>
-        jsonlite::fromJSON(simplifyDataFrame = FALSE) |>
-        data.table::rbindlist(fill = TRUE) |>
-        as.data.frame()
-    )
-  } else if (query == "time_off/types" | query == "time_off/policies") {
-    httr::content(response, as = "text", type = "json", encoding = "UTF-8") |>
-      jsonlite::fromJSON(simplifyDataFrame = TRUE) |>
-      as.data.frame()
-  } else {
-    (return(response))
-  }
+  #browser()
+  # if (query == "fields") {
+  #   return(
+  #     response %>%
+  #       jsonlite::fromJSON(simplifyDataFrame = TRUE)
+  #   )
+  # } else if (query == "users") {
+  #   return(
+  #     response %>%
+  #       jsonlite::fromJSON(simplifyDataFrame = TRUE) %>%
+  #       data.table::rbindlist(fill = TRUE) %>%
+  #       as.data.frame()
+  #   )
+  # } else if (query == "tables") {
+  #   tbl <- response %>%
+  #     jsonlite::fromJSON(simplifyDataFrame = FALSE)
+  #
+  #   return(
+  #     purrr::map_chr(tbl, "alias") %>%
+  #       tibble::tibble() %>%
+  #       purrr::set_names("parent_alias")%>%
+  #       dplyr::mutate(fields = purrr::map(tbl, "fields")) %>%
+  #       tidyr::unnest_longer(fields) %>%
+  #       tidyr::unnest_wider(col = "fields")
+  #   )
+  # } else if (query == "lists") {
+  #   return(
+  #     response %>%
+  #       jsonlite::fromJSON(simplifyDataFrame = FALSE) %>%
+  #       data.table::rbindlist(fill = TRUE) %>%
+  #       as.data.frame()
+  #   )
+  # } else if (query == "time_off/types" | query == "time_off/policies") {
+  #   response %>%
+  #     jsonlite::fromJSON(simplifyDataFrame = TRUE) %>%
+  #     as.data.frame()
+  # } else {
+  #   (return(response))
+  # }
 }
