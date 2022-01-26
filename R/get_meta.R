b9538bc81d46b62559ce659f721010937df6f6d9
Andrew Little <alittle@ascent.io>
2022-01-26 11:16:24 +0000

    Tidy up get_meta

--- R/get_meta.R
+++ R/get_meta.R
@@ -1,28 +1,13 @@
-#' @title Bamboo API get request wrapper for BambooHR Account Information
-#'
-#' @description  Submits a get request to retrieve account meta information that
-#'  can be used to help discover the types of field/table ect. available in your account
-#'devto
-#' @param query One of: "fields", "tables", "lists", "users", "time_off/types", "time_off/policies".
-#' @param url_args (optional) - A list of named or unnamed arguments to be passed to build_URL function.
-#'
-#' @return data.frame
-#'
-#' @export
-#' @examples
-#' \dontrun{
-#' res <- get_meta("fields")
-#'
-#' res <- get_meta("users", list(company_domain = "CompanyB"))
-#' }
-#'
-#' @references \url{https://documentation.bamboohr.com/reference/account-information-1}
 get_meta <- function(query = c("fields", "tables", "lists", "users", "time_off/types", "time_off/policies"), url_args = NULL) {
   if (!is.null(url_args)) {
     stopifnot(is.list(url_args))
   }
 
+
+
   if (length(query) > 1) stop('query must be one of c("fields", "tables", "lists", "users", "time_off/types", "time_off/policies")')
+
+
 
   if (!is.null(url_args)) {
     stopifnot(is.list(url_args))
@@ -32,7 +17,7 @@ get_meta <- function(query = c("fields", "tables", "lists", "users", "time_off/t
   url <- do.call(build_url, url_args)
   url <- glue::glue("{url}/meta/{query}")
 
-  browser()
+  # Create response object depending on query
   response <- get_request(url) %>%
     httr::content(response, as = "text", type = "json", encoding = "UTF-8") %>%
     purrr::when(
@@ -41,51 +26,22 @@ get_meta <- function(query = c("fields", "tables", "lists", "users", "time_off/t
     ) %>%
     purrr::when(
       query == "tables" ~ {function(x) {purrr::map_chr(., "alias") %>%
-        tibble::tibble(.) %>%
-        purrr::set_names("parent_alias")%>%
-        dplyr::mutate(fields = purrr::map(x, "fields")) %>%
-        tidyr::unnest_longer(fields) %>%
-        tidyr::unnest_wider(col = "fields")}}(),
+          tibble::tibble(.) %>%
+          purrr::set_names("parent_alias") %>%
+          dplyr::mutate(fields = purrr::map(x, "fields")) %>%
+          tidyr::unnest_longer(fields) %>%
+          tidyr::unnest_wider(col = "fields")}}(.),
+      query == "lists" ~ purrr::map(., function(s) {
+        s$fieldId <- as.character(s$fieldId)
+        s
+      }) %>%
+        dplyr::bind_rows()
+        ,
       ~ dplyr::bind_rows(.)
-    )
+    ) %>%
+    tibble::tibble()
 
-  #browser()
-  # if (query == "fields") {
-  #   return(
-  #     response %>%
-  #       jsonlite::fromJSON(simplifyDataFrame = TRUE)
-  #   )
-  # } else if (query == "users") {
-  #   return(
-  #     response %>%
-  #       jsonlite::fromJSON(simplifyDataFrame = TRUE) %>%
-  #       data.table::rbindlist(fill = TRUE) %>%
-  #       as.data.frame()
-  #   )
-  # } else if (query == "tables") {
-  #   tbl <- response %>%
-  #     jsonlite::fromJSON(simplifyDataFrame = FALSE)
-  #
-  #   return(
-  #     purrr::map_chr(tbl, "alias") %>%
-  #       tibble::tibble() %>%
-  #       purrr::set_names("parent_alias")%>%
-  #       dplyr::mutate(fields = purrr::map(tbl, "fields")) %>%
-  #       tidyr::unnest_longer(fields) %>%
-  #       tidyr::unnest_wider(col = "fields")
-  #   )
-  # } else if (query == "lists") {
-  #   return(
-  #     response %>%
-  #       jsonlite::fromJSON(simplifyDataFrame = FALSE) %>%
-  #       data.table::rbindlist(fill = TRUE) %>%
-  #       as.data.frame()
-  #   )
-  # } else if (query == "time_off/types" | query == "time_off/policies") {
-  #   response %>%
-  #     jsonlite::fromJSON(simplifyDataFrame = TRUE) %>%
-  #     as.data.frame()
-  # } else {
-  #   (return(response))
-  # }
+  return(response)
+
+
 }
