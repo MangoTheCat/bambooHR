b59e62199cfb177ac2cfce09ea180d8472c07b5a
tfry <tim.fry@mango-solutions.com>
2022-01-26 16:28:38 +0000

    replaced url_args argument with api_version and base_url

--- R/get_meta.R
+++ R/get_meta.R
@@ -1,47 +1,71 @@
-get_meta <- function(query = c("fields", "tables", "lists", "users", "time_off/types", "time_off/policies"), url_args = NULL) {
-  if (!is.null(url_args)) {
-    stopifnot(is.list(url_args))
-  }
+#' @title Bamboo API get request wrapper for BambooHR Account Information
+#'
+#' @description  Submits a get request to retrieve account meta information that
+#'  can be used to help discover the types of field/table ect. available in your account
+#' devto
+#' @param query One of: "fields", "tables", "lists", "users", "time_off/types", "time_off/policies".
+#' @param api_version (optional) - Version of API to use to make request. Default is "v1".
+#' @param base_url (optional) - URL to BambooHR API. Default is "https://api.bamboohr.com/api/gateway.php".
+#'
+#' @return A [tibble::tibble()] object.
+#' @md
+#' @export
+#' @examples
+#' \dontrun{
+#' res <- get_meta("fields")
+#'
+#' res2 <- get_meta("users")
+#' }
+#'
+#' @references \url{https://documentation.bamboohr.com/reference/account-information-1}
+
+get_meta <- function(query = c(
+                       "fields", "tables", "lists", "users",
+                       "time_off/types", "time_off/policies"
+                     ),
+                     api_version = "v1",
+                     base_url = "https://api.bamboohr.com/api/gateway.php") {
+  stopifnot(query %in% c(
+    "fields", "tables", "lists", "users",
+    "time_off/types", "time_off/policies"
+  ))
+
+  if (length(query) > 1) stop('query must be one of c("fields", "tables","lists",
+                              "users", "time_off/types", "time_off/policies")')
 
 
-
-  if (length(query) > 1) stop('query must be one of c("fields", "tables", "lists", "users", "time_off/types", "time_off/policies")')
-
-
-
-  if (!is.null(url_args)) {
-    stopifnot(is.list(url_args))
-  } else {
-    url_args <- list()
-  }
-  url <- do.call(build_url, url_args)
+  url <- build_url(
+    api_version = api_version,
+    base_url = base_url
+  )
   url <- glue::glue("{url}/meta/{query}")
 
   # Create response object depending on query
   response <- get_request(url) %>%
     httr::content(response, as = "text", type = "json", encoding = "UTF-8") %>%
     purrr::when(
-      query %in% c("fields", "users", "time_off/types", "time_off/policies") ~ jsonlite::fromJSON(. , simplifyDataFrame = TRUE),
-      query %in% c("tables", "lists") ~ jsonlite::fromJSON(. , simplifyDataFrame = FALSE)
+      query %in% c("fields", "users", "time_off/types", "time_off/policies") ~ jsonlite::fromJSON(., simplifyDataFrame = TRUE),
+      query %in% c("tables", "lists") ~ jsonlite::fromJSON(., simplifyDataFrame = FALSE)
     ) %>%
     purrr::when(
-      query == "tables" ~ {function(x) {purrr::map_chr(., "alias") %>%
-          tibble::tibble(.) %>%
-          purrr::set_names("parent_alias") %>%
-          dplyr::mutate(fields = purrr::map(x, "fields")) %>%
-          tidyr::unnest_longer(fields) %>%
-          tidyr::unnest_wider(col = "fields")}}(.),
+      query == "tables" ~ {
+        function(x) {
+          purrr::map_chr(., "alias") %>%
+            tibble::tibble(.) %>%
+            purrr::set_names("parent_alias") %>%
+            dplyr::mutate(fields = purrr::map(x, "fields")) %>%
+            tidyr::unnest_longer(fields) %>%
+            tidyr::unnest_wider(col = "fields")
+        }
+      }(.),
       query == "lists" ~ purrr::map(., function(s) {
         s$fieldId <- as.character(s$fieldId)
         s
       }) %>%
-        dplyr::bind_rows()
-        ,
+        dplyr::bind_rows(),
       ~ dplyr::bind_rows(.)
     ) %>%
     tibble::tibble()
 
   return(response)
-
-
 }
