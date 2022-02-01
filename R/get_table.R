00ea163dd07e77bab481415d2287e89ec0ddc416
tfry <tim.fry@mango-solutions.com>
2022-02-01 12:40:55 +0000

    Update function body and docs

--- R/get_table.R
+++ R/get_table.R
@@ -1,34 +1,34 @@
 #' @title Bamboo API get request wrapper for BambooHR tables
 #'
-#' @description  Submits a get request to retrieve a specified table from the system, valid tables are retreived from the
-#' meta data API
+#' @description Returns a data structure representing all the table rows for
+#'  a given employee and table combination.
 #'
-#' @param user Bamboo api user id, register in Bamboo "API Keys"
-#' @param password Bamboo login password
-#' @param verbose a logical; indicates if detailed output from httr calls should be provided; default FALSE
-#' @param table_alias the a alis of the table name requested, must be valid from the meta data API
-#' @return tbl_df
+#' @param employee_id Character of employee ID to return. A special employee ID of "all",
+#'  can also be used which will cause the API to return all rows for all employees in the table.
+#'   When specifying "all" employees, the result will include inactive and terminated employees.
+#' @param table_name Character of table name. Valid table names can be found in
+#' the "table_alias" column return by \code{get_meta("tables")}.
+#' @param api_version (optional) - Version of API to use to make request. Default is "v1".
 #'
+#' @return A [tibble::tibble()] object.
+#' @md
 #' @references \url{https://documentation.bamboohr.com/reference#get-employee-table-row-1}
 #'
 #' @export
 #'
 
-get_table <- function(table, id, since, ...) {
-  dots <- rlang::list2(...)
-  query <- NULL
-  if (rlang::is_missing(since)) {
-    id <- rlang::maybe_missing(id, default = "all")
-  } else {
-    id <- "changed"
-    query$since <- as_ISO8601_character(since)
-  }
+get_table <- function(employee_id, table_name, api_version = "v1") {
+
   url <- build_url(
-    company_domain = dots$company_domain,
-    api_version = dots$api_version,
-    base_url = dots$base_url
+    api_version = api_version,
   )
-  url <- glue::glue("{url}/employees/{id}/tables/{table}") |>
-    httr::modify_url(query = query)
+  url <- glue::glue("{url}/employees/{employee_id}/tables/{table_name}")
   response <- get_request(url)
+
+  return(
+  response %>%
+    httr::content(as='text', type='json', encoding='UTF-8') %>%
+    jsonlite::fromJSON(simplifyDataFrame=TRUE) %>%
+    tibble::tibble()
+  )
 }
