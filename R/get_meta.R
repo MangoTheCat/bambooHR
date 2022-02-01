9c32f362eda11286df1f746e71104383604b4356
Andrew Little <alittle@ascent.io>
2022-02-01 10:50:36 +0000

    Fixing get_meta. New comments, unnesting 'lists' which returned a column as a list, cleaning column names, updating documentation

--- R/get_meta.R
+++ R/get_meta.R
@@ -1,13 +1,13 @@
-#' @title Bamboo API get request wrapper for BambooHR Account Information
+#' @title Get Meta Information
 #'
 #' @description  Submits a get request to retrieve account meta information that
-#'  can be used to help discover the types of field/table ect. available in your account
+#'  can be used to help discover the types of field/table etc. available in your account.
 #'
 #' @param query One of: "fields", "tables", "lists", "users", "time_off/types", "time_off/policies".
 #' @param api_version (optional) - Version of API to use to make request. Default is "v1".
 #'
 #' @return A [tibble::tibble()] object.
-#' @md
+#'
 #' @export
 #' @examples
 #' \dontrun{
@@ -19,10 +19,10 @@
 #' @references \url{https://documentation.bamboohr.com/reference/account-information-1}
 
 get_meta <- function(query = c(
-                       "fields", "tables", "lists", "users",
-                       "time_off/types", "time_off/policies"
-                     ),
-                     api_version = "v1") {
+  "fields", "tables", "lists", "users",
+  "time_off/types", "time_off/policies"
+),
+api_version = "v1") {
 
   # Define query argument as default, if not given
   query <- match.arg(query)
@@ -31,20 +31,23 @@ get_meta <- function(query = c(
   if (length(query) > 1) stop('query must be one of c("fields", "tables","lists",
                               "users", "time_off/types", "time_off/policies")')
 
-
+  # Build up URL with query
   url <- build_url(
     api_version = api_version
   )
   url <- glue::glue("{url}/meta/{query}")
 
-  # Create response object depending on query
+  ## Create response object depending on query
   # Essentially we just convert to a nice-looking dataframe
+
+  # Get query and convert from JSON
   response <- get_request(url) %>%
     httr::content(response, as = "text", type = "json", encoding = "UTF-8") %>%
     purrr::when(
       query %in% c("fields", "users", "time_off/types", "time_off/policies") ~ jsonlite::fromJSON(., simplifyDataFrame = TRUE),
       query %in% c("tables", "lists") ~ jsonlite::fromJSON(., simplifyDataFrame = FALSE)
     ) %>%
+    # Depending on query type, use data manipulation to tabulate results
     purrr::when(
       query == "tables" ~ {
         function(x) {
@@ -60,10 +63,19 @@ get_meta <- function(query = c(
         s$fieldId <- as.character(s$fieldId)
         s
       }) %>%
-        dplyr::bind_rows(),
+        dplyr::bind_rows() %>%
+        # Fix name in nested "options" column so that we can unnest it
+        dplyr::mutate(options = lapply(options, function(ls) {
+          ls$value <- ls$name
+          ls$name <- NULL
+          ls
+        })) %>%
+        tidyr::unnest_wider("options"),
       ~ dplyr::bind_rows(.)
     ) %>%
-    tibble::tibble()
+    tibble::tibble() %>%
+    # Clean column names
+    janitor::clean_names()
 
   return(response)
 }
