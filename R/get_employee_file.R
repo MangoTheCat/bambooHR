103f5f0fbb6aa2942bb372f91b4305e0fb0d3532
Harry Alexander <harry.alexander@mango-solutions.com>
2022-02-10 12:47:03 +0000

    Finished changes to get_employee_file function

--- R/get_employee_file.R
+++ R/get_employee_file.R
@@ -1,22 +1,49 @@
-#' @title Bamboo HR API get request wrapper
+#' @title Retrieve An Employee File
 #'
-#' @description  Submits get requests to the Bamboo API
+#' @description `get_employee_file` takes 'id' (string), 'file_id', (string), and
+#' 'api_version' (string) and then requests and returns data about
+#' the corresponding file from the BambooHR API.
+#' The file will then be written to the user's working directory if possible.
 #'
-#' @param id employee ID. The special employee ID of zero (0) means to use the employee ID associated with the API key (if any).
-#' @param file_id  vector of values
-#' @param verbose Logical scalar. Should the function provide verbose messaging back on each step? - (XL) Sounds good
-#' @param ... tbd
+#' @param id The employee id of the employee.
+#' @param file_id The file id of the file to be returned.
+#' @param api_version The version of BambooHR API to be used.
 #'
-#' @return response object
-#' @author Mark Druffel, \email{mdruffel@propellerpdx.com}
-get_employee_file <- function(id, file_id, verbose, ...){
-  dots <- rlang::list2(...)
-  #Default to view if a file_id is not specified, this provides all files the employee has
+#' @return returns a response object.
+#'
+#' @examples
+#' response <- get_employee_file(
+#' id = 0,
+#' file_id = "480",
+#' api_version = "v1"
+#' )
+#'
+#' @author Harry Alexander, \email{harry.alexander@ascent.io}
+#'
+get_employee_file <- function(id, file_id,
+                              api_version = "v1") {
+  url <- build_url(api_version = api_version)
+  # default to 0 if employee id is not specified
+  id <- rlang::maybe_missing(id, default = "0")
+  # Default to directory if an individual employee is not specified
   file_id <- rlang::maybe_missing(file_id, default = "view")
-  url <- build_url(company_domain = dots$company_domain,
-                   api_version = dots$api_version,
-                   base_url = dots$base_url)
+  # Glues "/files/file_id" to url returned from build_url call
   url <- glue::glue("{url}/employees/{id}/files/{file_id}")
   response <- get_request(url)
-  return(response)
+  # Extract file name from 'content-disposition' variable
+  file_name <- substring(
+    text = response$headers$`content-disposition`,
+    first = 23,
+    last = nchar(response$headers$`content-disposition`) - 1
+  )
+  # Attempt to parse content of file as raw binary and write it in wd
+  tryCatch({
+    response %>%
+      httr::content(as = "raw") %>%
+      writeBin(file_name)
+  }, error = function(e)
+    warning("Failed to download file."),
+  finally = {
+    return(response)
+  })
 }
