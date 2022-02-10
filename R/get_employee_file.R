2a27481fa9a2aee92a35aac93d3bf8e96f4dd3eb
Harry Alexander <harry.alexander@mango-solutions.com>
2022-02-10 12:31:43 +0000

    Completed get_employee_file.R

--- R/get_employee_file.R
+++ R/get_employee_file.R
@@ -1,13 +1,13 @@
 #' @title Retrieve An Employee File
 #'
-#' @description `get_employee_file` takes file_id, (string), id (string) and
-#' url_args (list) and then requests
-#' and returns data about the corresponding file from the BambooHR API.
+#' @description `get_employee_file` takes 'id' (string), 'file_id', (string), and
+#' 'api_version' (string) and then requests and returns data about
+#' the corresponding file from the BambooHR API.
+#' The file will then be written to the user's working directory if possible.
 #'
-#' @param url_args A list containing the required arguments to call `build_url`.
-#' company domain (string), api version (string), and base_url (string).
-#' @param file_id The file id (string) of the file to be returned.
-#' @param id The employee id (string) of the employee.
+#' @param id The employee id of the employee.
+#' @param file_id The file id of the file to be returned.
+#' @param api_version The version of BambooHR API to be used.
 #'
 #' @return returns a response object.
 #'
@@ -15,19 +15,14 @@
 #' response <- get_employee_file(
 #' id = 0,
 #' file_id = "480",
-#' url_args = list("ascent", "v1",
-#' "https://api.bamboohr.com/api/gateway.php")
+#' api_version = "v1"
 #' )
 #'
-#' @author Mark Druffel, \email{mdruffel@propellerpdx.com}
+#' @author Harry Alexander, \email{harry.alexander@ascent.io}
 #'
 get_employee_file <- function(id, file_id,
-                             url_args = NULL){
-  if (!is.null(url_args)) {
-    stopifnot(is.list(url_args))
-  } else {
-    url_args <- list()
-  }url <- do.call(build_url, url_args)
+                              api_version = "v1") {
+  url <- build_url(api_version = api_version)
   # default to 0 if employee id is not specified
   id <- rlang::maybe_missing(id, default = "0")
   # Default to directory if an individual employee is not specified
@@ -35,5 +30,20 @@ get_employee_file <- function(id, file_id,
   # Glues "/files/file_id" to url returned from build_url call
   url <- glue::glue("{url}/employees/{id}/files/{file_id}")
   response <- get_request(url)
-  return(response)
+  # Extract file name from 'content-disposition' variable
+  file_name <- substring(
+    text = response$headers$`content-disposition`,
+    first = 23,
+    last = nchar(response$headers$`content-disposition`) - 1
+  )
+  # Attempt to parse content of file as raw binary and write it in wd
+  tryCatch({
+    response %>%
+      httr::content(as = "raw") %>%
+      writeBin(file_name)
+  }, error = function(e)
+    warning("Failed to download file."),
+  finally = {
+    return(response)
+  })
 }
