f3e9b31299553a2829123b191579459a709af7f4
Harry Alexander <harry.alexander@mango-solutions.com>
2022-02-22 11:20:57 +0000

    Minor changes to response_view.R

--- R/response_view.R
+++ R/response_view.R
@@ -1,9 +1,9 @@
 #' @title View Data About a Response.
 #'
 #' @description This function gets the `View` function from the utils package
-#' and prevents R studio from defaulting to their own viewer function when `View` is called.
+#' and thus prevents R studio from defaulting to their own viewer function when `View` is called.
 #'
-#' @param x an R object which can be coerced to a data frame with non-zero numbers of rows and columns.
+#' @param x An R object which can be coerced to a data frame with non-zero numbers of rows and columns.
 #' @param title Title for the viewer window.
 #'
 #' @return NULL.
@@ -12,5 +12,6 @@
 #  x <- response_view(x = df, title = "dataframe_title")
 #'}
 
-response_view <- function(x, title)
+response_view <- function(x, title){
   get("View", envir = as.environment("package:utils"))(x, title)
+}
