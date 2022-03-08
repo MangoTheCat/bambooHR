9b2f50952b143a1a6a7b4b9b5638403025deb551
Tom Bowling <tom.bowling@ascent.io>
2022-03-08 17:00:15 +0000

    add test helper for configuring summy config file, use withr::defer to clean up after tests complete

---
+++ R/test-helpers.R
@@ -0,0 +1,10 @@
+local_config <- function(env = parent.frame()) {
+  config_file <- .get_config_file()
+
+  withr::defer(use_config(config_file, verbose = FALSE), envir = env)
+
+  config_setup(apikey = "test", companyname = "ascent",
+               conffile = ".bambooconf.json", verbose = FALSE)
+
+  withr::defer(fs::file_delete(".bambooconf.json"), envir = env)
+}
