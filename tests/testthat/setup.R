13eb819251d46c026ca7ef688ed8e5bfb8e8be50
Tom Bowling <tom.bowling@ascent.io>
2022-03-08 17:01:14 +0000

    simplify test setup as now in test helpers

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -1,9 +1,2 @@
 library(httptest)
 
-# config_file <- .get_config_file()
-#
-# withr::defer({
-#   use_config(config_file)
-# })
-#
-# config_setup(apikey = "test", companyname = "ascent", conffile = ".bambooconf.json")
