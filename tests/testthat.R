9482ca00e138b1223c4de7597c3fdc0a479f3280
Tom Bowling <tom.bowling@ascent.io>
2022-03-08 15:47:21 +0000

    move config_setup steps to setup.R rather than testthat.R

--- tests/testthat.R
+++ tests/testthat.R
@@ -2,4 +2,3 @@ library(testthat)
 library(bambooHR)
 
 test_check("bambooHR")
-config_setup(apikey = "test", companyname = "ascent", conffile = ".bambooconf.json")
--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -1,1 +1,2 @@
 library(httptest)
+config_setup(apikey = "test", companyname = "ascent", conffile = ".bambooconf.json")
