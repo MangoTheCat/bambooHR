c6f93c25eb30448d13f8c6d4135d9919be12390c
Tom Bowling <tom.bowling@ascent.io>
2022-03-09 10:18:57 +0000

    update test helper to just use local options

--- R/test-helpers.R
+++ R/test-helpers.R
@@ -1,11 +1,12 @@
 local_config <- function(env = parent.frame()) {
-  config_file <- .get_config_file()
-
-  withr::defer(use_config(config_file, verbose = FALSE), envir = env)
-
-  config_setup(apikey = "test", companyname = "ascent",
-               conffile = "bambooconf.json", verbose = FALSE)
-
-  #withr::defer(fs::file_delete("bambooconf.json"), envir = env)
+  # config_file <- .get_config_file()
+  #
+  # withr::defer(use_config(config_file, verbose = FALSE), envir = env)
+  #
+  # config_setup(apikey = "test", companyname = "ascent",
+  #              conffile = "bambooconf.json", verbose = FALSE)
+  #
+  # #withr::defer(fs::file_delete("bambooconf.json"), envir = env)
+  withr::local_options(list("bambooHR.api_key" = "api_key", "bambooHR.company_name" = "ascent"))
 
 }
