af1e64eba4406e87b9238d045f2cef7467735eef
Tom Bowling <tom.bowling@ascent.io>
2022-03-09 10:59:09 +0000

    try another approach to local options

--- R/test-helpers.R
+++ R/test-helpers.R
@@ -1,13 +1,6 @@
 local_config <- function(env = parent.frame()) {
 
-  reslist <- list(api_key = "apikey", company_name = "ascent")
-  json <- jsonlite::toJSON(reslist, auto_unbox=TRUE, pretty=TRUE)
-
-  f <- file("~/bambooHR_user_config.json",open = "w")
-  cat(json,file = f)
-  close(f)
-
-  withr::local_options("bambooHR.config_file" = "~/bambooHR_user_config.json")
-  withr::defer_parent(unlink("~/bambooHR_user_config.json"))
+  op <- options("bambooHR.api_key" = "apikey", "bambooHR.company_name" = "ascent")
+  defer(options(op), env = env)
 
 }
