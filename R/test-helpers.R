734daa1d3545abb5f68ff348fd4a0c2ace168c7f
Tom Bowling <tom.bowling@ascent.io>
2022-03-09 10:36:04 +0000

    create json file direclty in test helper

--- R/test-helpers.R
+++ R/test-helpers.R
@@ -1,12 +1,12 @@
 local_config <- function(env = parent.frame()) {
-  # config_file <- .get_config_file()
-  #
-  # withr::defer(use_config(config_file, verbose = FALSE), envir = env)
-  #
-  # config_setup(apikey = "test", companyname = "ascent",
-  #              conffile = "bambooconf.json", verbose = FALSE)
-  #
-  # #withr::defer(fs::file_delete("bambooconf.json"), envir = env)
-  withr::local_options(list("bambooHR.api_key" = "api_key", "bambooHR.company_name" = "ascent"))
+
+  reslist <- list(api_key = "apikey", company_name = "ascent")
+  json <- jsonlite::toJSON(reslist, auto_unbox=TRUE, pretty=TRUE)
+
+  f <- file("~/bambooHR_user_config.json",open = "w")
+  cat(json,file = f)
+  close(f)
+
+  withr::defer_parent(unlink("~/bambooHR_user_config.json"))
 
 }
