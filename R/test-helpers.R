94f2331165239de4aa9b69cb68eadea9c9923255
Tom Bowling <tom.bowling@ascent.io>
2022-03-09 08:05:18 +0000

    remove . from temporary config file name in test helpers

--- R/test-helpers.R
+++ R/test-helpers.R
@@ -4,7 +4,7 @@ local_config <- function(env = parent.frame()) {
   withr::defer(use_config(config_file, verbose = FALSE), envir = env)
 
   config_setup(apikey = "test", companyname = "ascent",
-               conffile = ".bambooconf.json", verbose = FALSE)
+               conffile = "bambooconf.json", verbose = FALSE)
 
-  withr::defer(fs::file_delete(".bambooconf.json"))
+  withr::defer(fs::file_delete("bambooconf.json"))
 }
