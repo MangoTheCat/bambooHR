bc03ea121393d707ccc14e807171f6b9979f32c3
Tom Bowling <tom.bowling@ascent.io>
2022-03-09 11:06:52 +0000

    add withr namespacing to use of defer

--- R/test-helpers.R
+++ R/test-helpers.R
@@ -1,6 +1,6 @@
 local_config <- function(env = parent.frame()) {
 
   op <- options("bambooHR.api_key" = "apikey", "bambooHR.company_name" = "ascent")
-  defer(options(op), env = env)
+  withr::defer(options(op), env = env)
 
 }
